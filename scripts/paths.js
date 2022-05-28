8648b20cd30d2fce64106b4ac0712b1a78c0f4f2
AeGGIndie <37030567+AeGGIndie@users.noreply.github.com>
2022-05-28 01:07:31 -0400

    removed the request script and parsers to @uogCannonBot/cannon-parsers . also updated the package.json

--- scripts/paths.js
+++
@@ -1,8 +0,0 @@
-// path for thecannon and house listings
-const cannonPath = "https://www.thecannon.ca";
-const housePath = cannonPath + "/classifieds/housing";
-
-module.exports = {
-  cannonPath,
-  housePath,
-};
